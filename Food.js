5469914a6ec1d374f66cc41068ee6dce9e6334fd
vishal-naveen <71558475+vishal-naveen@users.noreply.github.com>
2021-06-07 13:25:23 -0400

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,143 @@
+class Food {
+    constructor(){
+    this.foodStock=0;
+    this.lastFed;
+    this.image=loadImage('Milk.png');
+    }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
+    }
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
+    display(){
+      var x=80,y=100;
+      
+      imageMode(CENTER);
+      image(this.image,720,220,70,70);
+      
+      if(this.foodStock!=0){
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
+        }
+      }
+    }
+}
+
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+
+//create feed and lastFed variable here
+var feed, lastFed
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+  feedthedog = createButton("Feed the Dog")
+  feedthedog.position(700,95)
+  feedthedog.mousePressed(feedDog)
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  fedTime=database.ref('FeedTime')
+  fedTime.on("value",function(data){
+  lastFed=data.val()
+  })
+  
+ 
+  //write code to display text lastFed time here
+
+  fill(255,255,255)
+
+  if(lastFed>=12){
+    text("Last Feed: "+ lastFed%12+"pm",350,30)
+  }else if(lastFed==0){
+    text("Last Feed: 12 AM",350,30)
+  }else{
+    text("Last Feed: "+ lastFed+"am",350,30)
+  }
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  //write code here to update food stock and last fed time
+  var food_stock_val = foodObj.getFoodStock();
+  if(food_stock_val <= 0){
+    foodObj.updateFoodStock(food_stock_val * 0);
+  }else{
+    foodObj.updateFoodStock(food_stock_val -1);
+  }
+
+  database.ref('/').update({
+    Food:foodObj.updateFoodStock(),
+    FeedTime: hour()
+  })
+
+}
+
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+
